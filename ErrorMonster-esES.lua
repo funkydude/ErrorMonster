b6466f1958ef0a9b2cb81a00d2eb800484cdc35e
Vidar Haarr <vhaarr@gmail.com>
2006-12-13 00:05:27 +0000

    ErrorMonster: Spanish translation, thanks to shiftos.

---
+++ ErrorMonster-esES.lua
@@ -0,0 +1,35 @@
+local L = AceLibrary("AceLocale-2.2"):new("ErrorMonster")
+
+L:RegisterTranslations("esES", function() return {
+	["Shows the current filters and their ID."] = "Muestra los filtros actuales y su ID.",
+	["Adds the given filter to the ignore list."] = "A\195\177ade el filtro a la lista de ignorados.",
+	["<filter>"] = "<filtro>",
+	["Removes the given filter or ID from the filter list."] = "Elimina el filtro o ID de la lista de filtros.",
+	["Adding filter: "] = "A\195\177adiendo filtro: ",
+	["Removing filter: "] = "Eliminando filtro: ",
+	["Filter not found: "] = "Filtro no encontrado: ",
+	["Active filters:"] = "Filtros activos: ",
+	["Where to flush the messages matched by the filters."] = "Donde lanzar los mensajes que concuerdan con los filtros.",
+	["Monster"] = "Monstruo",
+	["Throttle errors at the given rate in seconds."] = "Acelerar los errores al ritmo determinado en segundos.",
+	["Go berserk and eat all the errors."] = "Volverse berserker y comerse todos los errores.",
+	["Teach your ErrorMonster to aggro on other message types."] = "Ense\195\177ar a tu ErrorMonster a aggrear a otros tipos de mensajes.",
+	["Error messages."] = "Mensajes de error.",
+	["Information messages."] = "Mensajes de informaci\195\179n.",
+	["System messages."] = "Mensajes de sistema.",
+	["System messages"] = "Mensajes de sistema",
+	["|cffff0000Ignore|r"] = "|cffff0000Ignorar|r",
+	["|cff00ff00Aggro|r"] = true,
+
+	["list"] = "lista",
+	["add"] = "a\195\177adir",
+	["remove"] = "eliminar",
+	["sink"] = "hundir",
+	["throttle"] = "acelerar",
+	["berserk"] = "berserker",
+	["aggro"] = true,
+	["error"] = true,
+	["information"] = "informaci\195\179n",
+	["system"] = "sistema",
+} end)
+
