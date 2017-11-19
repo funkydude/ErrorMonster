794ed43ee510c9bdf65846462f098c49713bbc55
funkydude <funkydude@users.noreply.github.com>
2017-11-19 15:55:22 +0000

    add locale

--- Locales/deDE.lua
+++ Locales/deDE.lua
@@ -2,5 +2,16 @@
 if GetLocale() ~= "deDE" then return end
 local _, tbl = ...
 local L = tbl.L
---@localization(locale="deDE", format="lua_additive_table", handle-unlocalized="ignore", table-name="L")@
 
+L.addon_desc1 = "ErrorMonster ist ein Begleiter, der Fehler, System- und Informationsmeldungen aus dem UI-Fehler-Fenster drosselt und zu vielen anderen Ausgabenmöglichkeiten, einschließlich anderen Addons, weiterleitet."
+L.addon_desc2 = "Um einzustellen, wohin ErrorMonster die Ausgaben umleiten soll, erweitere den Pfadbaum an der Seite und gehe zum Bereich Ausgabe."
+L.addon_desc3 = "Unterhalb kannst du umschalten, welche Art von Meldungen abgefangen werden sollen. Standardmäßig handhabt ErrorMonster nur tatsächliche Fehler (die roten)."
+L.output = "Ausgabe"
+L.error = "Fehler"
+L.error_desc = "Fehlermeldungen"
+L.information = "Informationen"
+L.information_desc = "Informationsmeldungen"
+L.system = "System"
+L.system_desc = "Systemmeldungen"
+L.combat = "Alle Meldungen während des Kampfes verstecken."
+L.combat_desc = "Blendet alle abgefangenen Meldungen aus, während du dich im Kampf befindest."
--- Locales/frFR.lua
+++ Locales/frFR.lua
@@ -2,5 +2,16 @@
 if GetLocale() ~= "frFR" then return end
 local _, tbl = ...
 local L = tbl.L
---@localization(locale="frFR", format="lua_additive_table", handle-unlocalized="ignore", table-name="L")@
 
+L.addon_desc1 = "ErrorMonster est votre compagnon qui étouffe et redirige les erreurs, les messages système et les messages d'informations venant de l'UI et pleins d'autres sources, ce qui inclue aussi les autres addons"
+L.addon_desc2 = "Pour configurer où ErrorMonster doit rediriger les messages,veuillez étendre l'onglet dans les coins et aller dans la section de Sortie"
+L.addon_desc3 = "Vous pouvez ci-dessous choisir quel genre de messages vous voulez intercepter. Par défaut,ErrorMonster s'occupera seulement des erreurs actuel(les rouges)"
+L.output = "Sortie"
+L.error = "Erreur"
+L.error_desc = "Messages d'erreur"
+L.information = "Information"
+L.information_desc = "Messages d'information"
+L.system = "Système"
+L.system_desc = "Messages Système"
+L.combat = "Cache tous les messages en combat"
+L.combat_desc = "Cache tous les messages interceptés quand vous êtes en combat"
