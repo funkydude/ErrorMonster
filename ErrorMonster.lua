41917a1cd399e0168c1e608c36fbe3c2ebf9f3e4
funkydude <funkydude@users.noreply.github.com>
2026-01-21 16:50:11 +0000

    Cleanup

--- ErrorMonster.lua
+++ ErrorMonster.lua
@@ -1,5 +1,4 @@
 local _, addon = ...
-local frame = CreateFrame("Frame")
 local L = addon.L
 LibStub("LibSink-2.0"):Embed(addon)
 
@@ -44,77 +43,75 @@ do
 	end)
 end
 
-frame:SetScript("OnEvent", function(self, event, addonName)
-	if addonName == "ErrorMonster" then
-		addon.db = LibStub("AceDB-3.0"):New("ErrorMonsterDB", {
-			profile = {
-				sink20OutputSink = "UIErrorsFrame",
-				errors = true,
-				information = false,
-				system = false,
-				combat = false,
+do
+	addon.db = LibStub("AceDB-3.0"):New("ErrorMonsterDB", {
+		profile = {
+			sink20OutputSink = "UIErrorsFrame",
+			errors = true,
+			information = false,
+			system = false,
+			combat = false,
+		},
+	}, true)
+
+	local args = {
+		type = "group",
+		handler = addon,
+		get = function(info) return addon.db.profile[info[1]] end,
+		set = function(info, v) addon.db.profile[info[1]] = v end,
+		args = {
+			desc = {
+				type = "description",
+				name = L.addon_desc1.."\n\n"..L.addon_desc2.."\n\n"..L.addon_desc3.."\n\n",
+				order = 1,
+				fontSize = "medium",
 			},
-		}, true)
-
-		local args = {
-			type = "group",
-			handler = addon,
-			get = function(info) return addon.db.profile[info[1]] end,
-			set = function(info, v) addon.db.profile[info[1]] = v end,
-			args = {
-				desc = {
-					type = "description",
-					name = L.addon_desc1.."\n\n"..L.addon_desc2.."\n\n"..L.addon_desc3.."\n\n",
-					order = 1,
-					fontSize = "medium",
-				},
-				errors = {
-					type = "toggle",
-					name = L.error,
-					desc = L.error_desc,
-					order = 2,
-					width = "full",
-				},
-				information = {
-					type = "toggle",
-					name = L.information,
-					desc = L.information_desc,
-					order = 3,
-					width = "full",
-				},
-				system = {
-					type = "toggle",
-					name = L.system,
-					desc = L.system_desc,
-					order = 4,
-					width = "full",
-				},
-				spacer = {
-					type = "description",
-					name = "\n",
-					order = 10,
-				},
-				combat = {
-					type = "toggle",
-					name = L.combat,
-					desc = L.combat_desc,
-					order = 20,
-					width = "full",
-				},
+			errors = {
+				type = "toggle",
+				name = L.error,
+				desc = L.error_desc,
+				order = 2,
+				width = "full",
 			},
-		}
-		addon:SetSinkStorage(addon.db.profile)
+			information = {
+				type = "toggle",
+				name = L.information,
+				desc = L.information_desc,
+				order = 3,
+				width = "full",
+			},
+			system = {
+				type = "toggle",
+				name = L.system,
+				desc = L.system_desc,
+				order = 4,
+				width = "full",
+			},
+			spacer = {
+				type = "description",
+				name = "\n",
+				order = 10,
+			},
+			combat = {
+				type = "toggle",
+				name = L.combat,
+				desc = L.combat_desc,
+				order = 20,
+				width = "full",
+			},
+		},
+	}
+	addon:SetSinkStorage(addon.db.profile)
 
-		LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("ErrorMonster", args)
-		LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("ErrorMonster-Output", function() return addon:GetSinkAce3OptionsDataTable() end)
-		LibStub("AceConfigDialog-3.0"):AddToBlizOptions("ErrorMonster", "ErrorMonster")
-		LibStub("AceConfigDialog-3.0"):AddToBlizOptions("ErrorMonster-Output", L.output, "ErrorMonster")
+	LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("ErrorMonster", args)
+	LibStub("AceConfigRegistry-3.0"):RegisterOptionsTable("ErrorMonster-Output", function() return addon:GetSinkAce3OptionsDataTable() end)
+	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("ErrorMonster", "ErrorMonster")
+	LibStub("AceConfigDialog-3.0"):AddToBlizOptions("ErrorMonster-Output", L.output, "ErrorMonster")
+end
 
-		self:UnregisterEvent(event)
-		self:SetScript("OnEvent", function() throttle = {} end)
-		self:RegisterEvent("PLAYER_ENTERING_WORLD")
-	end
+local frame = CreateFrame("Frame")
+frame:SetScript("OnEvent", function()
+	throttle = {}
 end)
 
-frame:RegisterEvent("ADDON_LOADED")
-
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
